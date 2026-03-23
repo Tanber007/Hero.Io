9e74dcd3b603e44e0ac5cba5ba649e2f46f35f96
Tanber Ahmmed <tanzilahmmed007@gmail.com>
2026-03-23 15:07:20 +0600

    the search section is working

--- src/pages/Allapp/Allapp.jsx
+++ src/pages/Allapp/Allapp.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router';
 import SingleTrendApp from '../SingleTrendApp/SingleTrendApp';
 
 const Allapp = () => {
     const data = useLoaderData();
     // console.log(data);
+
+    const [search, setSearch] = useState('');
+    const filteredApps = data.filter(app => app.title.toLowerCase().includes(search.toLowerCase()));
 
     useEffect(()=>{
         window.scrollTo(0,0);
@@ -21,7 +24,7 @@ const Allapp = () => {
             <section className='w-11/12 mx-auto my-8'>
                 {/* total app & search section */}
                 <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4'>
-                    <h3 className='text-2xl font-bold'>({data.length})App Found</h3>
+                    <h3 className='text-2xl font-bold'>({filteredApps.length})App Found</h3>
                     {/* search */}
                     <label className="input">
                         <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -36,13 +39,13 @@ const Allapp = () => {
                                 <path d="m21 21-4.3-4.3"></path>
                             </g>
                         </svg>
-                        <input type="search" required placeholder="Search Apps" />
+                        <input value={search} onChange={(e) => setSearch(e.target.value)} type="search" required placeholder="Search Apps" />
                     </label>
                 </div>
                 {/* All App Card */}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                     {
-                        data.map(singleApp=> <SingleTrendApp key={singleApp.id} SingleTrendApp={singleApp}></SingleTrendApp>)
+                        filteredApps.map(singleApp=> <SingleTrendApp key={singleApp.id} SingleTrendApp={singleApp}></SingleTrendApp>)
                     }
                 </div>
             </section>
